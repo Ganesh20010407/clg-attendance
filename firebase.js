e1f89e315eb68e363751963d51a019368b96426e
Ganesh Bala <ganeshkodigudla047@gmail.com>
2026-01-23 17:48:04 +0530

    1.2.1

--- firebase.js
+++ firebase.js
@@ -1,9 +1,6 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth } from
-"https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getFirestore } from
-"https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAFUziq6QGKCwujtiTL-4Rk823FE12ZDGU",
